a302bcc14a0ce7a3c0153fbc6643297bbf4d324a
greenlinyu <93090978+greenlinyu@users.noreply.github.com>
2025-06-22 23:15:20 +0800

    Update app.js

--- alist/app.js
+++ alist/app.js
@@ -5,35 +5,25 @@ var exec = require("child_process").exec;
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 app.use('/', createProxyMiddleware({
-  target: 'http://127.0.0.1:port',
+  target: 'http://127.0.0.1:port', // 修改为自己端口
   changeOrigin: true,
   ws: true,
   onError: (err, req, res) => {
-    res.writeHead(500, {
-      'Content-Type': 'text/plain',
-    });
-    res.end('请稍等片刻后刷新页面');
+    res.writeHead(500, { 'Content-Type': 'text/plain' });
+    res.end('Alist 正在启动中，请稍候刷新。');
   },
 }));
 
 function keep_web_alive() {
   exec("pgrep -laf web.js", function (err, stdout, stderr) {
-    if (stdout.includes("web.js server")) {
-      console.log("web 正在运行");
-    } else {
-      exec(
-        "bash start.sh > /dev/null 2>&1 &",
-        function (err, stdout, stderr) {
-          if (err) {
-            console.log("保活失败:" + err);
-          } else {
-            console.log("保活：重新启动 web.js 成功！");
-          }
-        }
-      );
+    if (!stdout.includes("web.js server")) {
+      exec("bash start.sh", function (err) {
+        if (err) console.log("启动失败:", err);
+        else console.log("web.js 启动成功");
+      });
     }
   });
 }
-setInterval(keep_web_alive, 10 * 1000);
+setInterval(keep_web_alive, 10000);
 
-app.listen(port, () => console.log(`AList v2 反代服务启动于 ${port}`));
+app.listen(port, () => console.log(`反代服务运行在端口 ${port}`));
